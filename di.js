1f6a3bd1346d325e1941db5d7df93ebad854f66a
sobhan.mahmoudishamsabad <sobhanshms4106@gmail.com>
2025-07-04 22:48:15 +0330

    feat(di): post and comment di added.

--- di.js
+++ di.js
@@ -1,8 +1,17 @@
 import { createContainer, asClass, asValue } from 'awilix';
 
-import UserController from './controllers/userController.js';
+import UserController from './interfaces/http/user/userController.js';
+import PostController from './interfaces/http/post/postController.js';
+import CommentController from './interfaces/http/comment/commentController.js';
+
 import UserRepository from './src/domain/user/userRepository.js';
 import UserUsecaseRegistry from './src/application/user/userUsecaseRegistry.js';
+
+import PostRepository from './src/domain/post/postRepository.js';
+import PostUsecaseRegistry from './src/application/post/postUsecaseRegistry.js';
+
+import CommentRepository from './src/domain/comment/commentRepository.js';
+import CommentUsecaseRegistry from './src/application/comment/commentUsecaseRegistry.js';
 
 import serverConfig from './configs/serverConfig.js';
 
@@ -22,10 +31,18 @@ container.register({
 		}),
 
 	userRepository: asClass(UserRepository).scoped(),
+	postRepository: asClass(PostRepository).scoped(),
+	commentRepository: asClass(CommentRepository).scoped(),
 
 	userUsecaseRegistry: asClass(UserUsecaseRegistry).scoped(),
+	postUsecaseRegistry: asClass(PostUsecaseRegistry).scoped(),
+	commentUsecaseRegistry: asClass(CommentUsecaseRegistry).scoped(),
 
 	userController: asClass(UserController).scoped(),
+	postController: asClass(PostController).scoped(),
+	commentController: asClass(CommentController).scoped(),
+
+	logger: asValue(console),
 });
 
 export default container;
