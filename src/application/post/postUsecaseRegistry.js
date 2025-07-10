2c860aa9e31781bac426a65c7798e8a53742b013
sobhan.mahmoudishamsabad <sobhanshms4106@gmail.com>
2025-07-10 21:46:17 +0330

    feat(post): usecases updated.

--- src/application/post/postUsecaseRegistry.js
+++ src/application/post/postUsecaseRegistry.js
@@ -2,6 +2,8 @@ import AddPost from './addPost.js';
 import GetPostById from './getPostById.js';
 import GetPosts from './getPosts.js';
 import DeletePost from './deletePost.js';
+import UpdatePost from './updatePost.js';
+import GetPostComments from './getPostComments.js';
 
 class PostUsecaseRegistry {
 	constructor({ postRepository }) {
@@ -9,6 +11,8 @@ class PostUsecaseRegistry {
 		this.deletePost = new DeletePost(postRepository);
 		this.getPosts = new GetPosts(postRepository);
 		this.getPostById = new GetPostById(postRepository);
+		this.getPostComments = new GetPostComments(postRepository);
+		this.updatePost = new UpdatePost(postRepository);
 	}
 }
 
