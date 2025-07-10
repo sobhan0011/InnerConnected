97b4eb92309b58353f7719fb3fd34673cf208329
sobhan.mahmoudishamsabad <sobhanshms4106@gmail.com>
2025-07-10 21:48:47 +0330

    feat: repositories updated in di.

--- di.js
+++ di.js
@@ -1,38 +1,25 @@
 import { createContainer, asClass, asValue } from 'awilix';
 
+import serverConfig from './configs/serverConfig.js';
+
 import UserController from './interfaces/http/user/userController.js';
 import PostController from './interfaces/http/post/postController.js';
 import CommentController from './interfaces/http/comment/commentController.js';
 
-import UserRepository from './src/domain/user/userRepository.js';
 import UserUsecaseRegistry from './src/application/user/userUsecaseRegistry.js';
-
-import PostRepository from './src/domain/post/postRepository.js';
 import PostUsecaseRegistry from './src/application/post/postUsecaseRegistry.js';
-
-import CommentRepository from './src/domain/comment/commentRepository.js';
 import CommentUsecaseRegistry from './src/application/comment/commentUsecaseRegistry.js';
-
-import serverConfig from './configs/serverConfig.js';
 
 import Postgres from './src/infrastructure/db/postgres/postqres.js';
 import postgresConfig from './configs/moduleConfigs/postgresConfig.js';
+import postgresUserRepository from './src/infrastructure/db/postgres/postgresUserRepository.js';
+import postgresPostRepository from './src/infrastructure/db/postgres/postgresPostRepository.js';
+import postgresCommentRepository from './src/infrastructure/db/postgres/postgresCommentRepository.js';
 
 const container = createContainer();
 
 container.register({
 	serverConfig: asValue(serverConfig),
-	postgresConfig: asValue(postgresConfig),
-
-	db: asClass(Postgres)
-		.singleton()
-		.disposer((db) => {
-			return db.disconnect();
-		}),
-
-	userRepository: asClass(UserRepository).scoped(),
-	postRepository: asClass(PostRepository).scoped(),
-	commentRepository: asClass(CommentRepository).scoped(),
 
 	userUsecaseRegistry: asClass(UserUsecaseRegistry).scoped(),
 	postUsecaseRegistry: asClass(PostUsecaseRegistry).scoped(),
@@ -43,6 +30,20 @@ container.register({
 	commentController: asClass(CommentController).scoped(),
 
 	logger: asValue(console),
+});
+
+container.register({
+	postgresConfig: asValue(postgresConfig),
+
+	db: asClass(Postgres)
+		.singleton()
+		.disposer((db) => {
+			return db.disconnect();
+		}),
+
+	userRepository: asClass(postgresUserRepository).scoped(),
+	postRepository: asClass(postgresPostRepository).scoped(),
+	commentRepository: asClass(postgresCommentRepository).scoped(),
 });
 
 export default container;
