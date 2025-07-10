c61e40a8efeb61bd5a91fda6a8756ca935b11d1e
sobhan.mahmoudishamsabad <sobhanshms4106@gmail.com>
2025-07-10 21:48:56 +0330

    feat: validator middleware updated.

--- interfaces/http/common/middlewares/validator.js
+++ interfaces/http/common/middlewares/validator.js
@@ -1,7 +1,16 @@
-export function validate(schema, property) {
+export function validate(schema, property = 'body') {
 	return (req, res, next) => {
 		const data = req[property];
+
+		if (data === undefined || data === null) {
+			return res.status(400).json({
+				error: 'Validation failed',
+				details: `No ${property} provided.`,
+			});
+		}
+
 		const { error } = schema.validate(data, { abortEarly: false });
+
 		if (error) {
 			return res.status(400).json({
 				error: 'Validation failed',
@@ -10,6 +19,7 @@ export function validate(schema, property) {
 				}),
 			});
 		}
+
 		next();
 	};
 }
