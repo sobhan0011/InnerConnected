6194906f7e9c251a6f2f57d39c08debacfe265f3
sobhan.mahmoudishamsabad <sobhanshms4106@gmail.com>
2025-07-10 21:48:32 +0330

    feat: repository interfaces added.

--- src/domain/comment/commentRepository.js
+++ src/domain/comment/commentRepository.js
@@ -1,32 +1,11 @@
-class UserRepository {
-	constructor({ db }) {
-		this.db = db;
-	}
+class CommentRepository {
+	getAllComments() {}
 
-	async getAllUsers() {
-		const result = await this.db.query('SELECT * FROM users');
-		return result.rows;
-	}
+	getCommentById() {}
 
-	async getUserById(id) {
-		const result = await this.db.query('SELECT * FROM users WHERE id = $1', [id]);
-		return result.rows[0] || null;
-	}
+	addComment() {}
 
-	async createUser({ username, email, password }) {
-		const result = await this.db.query(
-			`INSERT INTO users (username, email, password)
-			 VALUES ($1, $2, $3)
-			 RETURNING *`,
-			[username, email, password],
-		);
-		return result.rows[0];
-	}
-
-	async deleteUser(id) {
-		const result = await this.db.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
-		return result.rows[0] || null;
-	}
+	deleteComment() {}
 }
 
-export default UserRepository;
+export default CommentRepository;
--- src/domain/post/postRepository.js
+++ src/domain/post/postRepository.js
@@ -1,32 +1,11 @@
-class UserRepository {
-	constructor({ db }) {
-		this.db = db;
-	}
+class PostRepository {
+	getAllPosts() {}
 
-	async getAllUsers() {
-		const result = await this.db.query('SELECT * FROM users');
-		return result.rows;
-	}
+	getPostById() {}
 
-	async getUserById(id) {
-		const result = await this.db.query('SELECT * FROM users WHERE id = $1', [id]);
-		return result.rows[0] || null;
-	}
+	addPost() {}
 
-	async createUser({ username, email, password }) {
-		const result = await this.db.query(
-			`INSERT INTO users (username, email, password)
-			 VALUES ($1, $2, $3)
-			 RETURNING *`,
-			[username, email, password],
-		);
-		return result.rows[0];
-	}
-
-	async deleteUser(id) {
-		const result = await this.db.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
-		return result.rows[0] || null;
-	}
+	deletePost() {}
 }
 
-export default UserRepository;
+export default PostRepository;
