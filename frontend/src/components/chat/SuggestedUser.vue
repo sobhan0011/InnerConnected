b21f950063a96de068d7ec688f84a0e4516de57f
sobhan0011 <sobhanshams7911@gmail.com>
2025-08-09 00:25:37 +0330

    feat(frontend): suggested users components added.

---
+++ frontend/src/components/chat/SuggestedUser.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="flex items-center gap-3 p-3 rounded-lg border border-gray-200 hover:bg-gray-100 cursor-pointer transition"
+  >
+    <img :src="user.profileImage" class="w-10 h-10 rounded-full object-cover" />
+    <span class="text-sm font-medium text-gray-800">{{ user.username }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Types
+import type { UserPreview } from '@/types/chat/userPreview';
+
+defineProps<{ user: UserPreview }>();
+</script>
---
+++ frontend/src/components/chat/SuggestedUsers.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="suggestedUsers.length" class="space-y-3">
+    <h3 class="text-sm text-gray-500">Suggestions</h3>
+    <SuggestedUser
+      v-for="user in suggestedUsers"
+      :key="user.id"
+      :user="user"
+      @click="emit('select-user', user)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+// Components
+import type { UserPreview } from '@/types/chat/userPreview';
+
+// Types
+import SuggestedUser from './SuggestedUser.vue';
+
+defineProps<{ suggestedUsers: UserPreview[] }>();
+const emit = defineEmits(['select-user']);
+</script>
