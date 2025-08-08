a16ee4fc6c6b39dc744820af72a993171df80b72
sobhan0011 <sobhanshams7911@gmail.com>
2025-08-09 00:22:31 +0330

    feat: getUsers authorization updated.

--- backend/src/application/user/usecases/getUsers.js
+++ backend/src/application/user/usecases/getUsers.js
@@ -1,6 +1,7 @@
 import { CustomError } from '../../../../errors/customError.js';
-import { ERROR_CODES } from '../../../../errors/erros.js';
+import { ERROR_CODES } from '../../../../errors/errors.js';
 import { UserResponseDto } from '../dtos/userResponseDto.js';
+import { UserLimitedResponseDto } from '../dtos/userLimitedResponseDto.js';
 import UserRoles from '../../../domain/user/userRoles.js';
 
 class GetUsers {
@@ -9,10 +10,6 @@ class GetUsers {
 	}
 
 	async execute(filters, requester) {
-		if (requester.role === UserRoles.USER) {
-			throw new CustomError(ERROR_CODES.UNAUTHORIZED);
-		}
-
 		filters = {
 			...filters,
 			role: UserRoles.USER,
@@ -21,9 +18,15 @@ class GetUsers {
 		const users = await this.userRepository.getUsers(filters);
 		if (!users) return [];
 
-		return users.map((user) => {
-			return new UserResponseDto(user);
-		});
+		if (requester.role === UserRoles.ADMIN) {
+			return users.map((user) => {
+				return new UserResponseDto(user);
+			});
+		} else if (requester.role === UserRoles.USER) {
+			return users.map((user) => {
+				return new UserLimitedResponseDto(user);
+			});
+		}
 	}
 }
 
